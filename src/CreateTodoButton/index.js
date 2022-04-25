7546c015e6f0d01a7c25be05b1bc850fe05280c1
carlirows <carlirows@gmail.com>
2022-04-25 10:31:45 -0500

    TodoApp-v1

---
+++ src/CreateTodoButton/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './CreateTodoButton.css';
+
+function CreateTodoButton({openModal, setOpenModal}) {
+
+
+  const onClickButton = () => {
+    setOpenModal((prevState) => !prevState)
+  };
+  
+  return (
+    <button
+      className="CreateTodoButton"
+      onClick={() => onClickButton()}
+    >
+      +
+    </button>
+  );
+}
+
+export { CreateTodoButton };
